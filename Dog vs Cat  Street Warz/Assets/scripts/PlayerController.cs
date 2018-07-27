5e9aaa91de6dca4750c842317640504f0a997e9c
Unknown <guessmiga@gmail.com>
2018-07-27 15:48:15 -0600

    assignment 2 Update

---
+++ Dog vs Cat  Street Warz/Assets/scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+    private void Start()
+    {
+        cam = FindObjectOfType<Camera>();
+    }
+
+    void Update ()
+    {
+        Touch myTouch = Input.GetTouch(0);
+        Touch[] touches = Input.touches;
+        for(int i = 0; i < Input.touchCount; i++)
+        {
+            //Do Something
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+	}
+}
